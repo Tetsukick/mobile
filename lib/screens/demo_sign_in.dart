02b6a0e4dd605c5facf0feae0a6a7909ab14d861
Ben Carlsson <qhiiyr@gmail.com>
2020-08-12 22:16:58 -0700

    Add demo sign in

---
+++ lib/screens/demo_sign_in.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:splitsio/models/auth.dart';
+import 'package:splitsio/models/runner.dart';
+import 'package:splitsio/screens/index.dart';
+
+import 'package:splitsio/widgets/game/run_list.dart';
+
+class DemoSignInScreen extends StatelessWidget {
+  DemoSignInScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Sign in with Splits.io")),
+      body: Column(children: [
+        SignInForm(),
+      ]),
+    );
+  }
+}
+
+class SignInForm extends StatefulWidget {
+  @override
+  SignInFormState createState() {
+    return SignInFormState();
+  }
+}
+
+// Create a corresponding State class.
+// This class holds data related to the form.
+class SignInFormState extends State<SignInForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a GlobalKey<FormState>,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+
+  final _usernameFocus = FocusNode();
+  final _passwordFocus = FocusNode();
+
+  void _submit() {
+    // Validate returns true if the form is valid, or false
+    // otherwise.
+    if (_formKey.currentState.validate()) {
+      // If the form is valid, display a Snackbar.
+      Scaffold.of(context).showSnackBar(SnackBar(content: Text('Signing in')));
+
+      Navigator.pushAndRemoveUntil(context,
+          MaterialPageRoute<void>(builder: (BuildContext context) {
+        Auth.demo = true;
+        return IndexScreen(runner: Runner.me());
+      }), (route) => false);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: TextFormField(
+              autocorrect: false,
+              autofocus: true,
+              decoration: InputDecoration(hintText: "Username"),
+              focusNode: _usernameFocus,
+              onFieldSubmitted: (term) {
+                _usernameFocus.unfocus();
+                FocusScope.of(context).requestFocus(_passwordFocus);
+              },
+              textInputAction: TextInputAction.next,
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter a username';
+                }
+                if (value != "demo") {
+                  return 'Incorrect username';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: TextFormField(
+              autocorrect: false,
+              autofocus: true,
+              decoration: InputDecoration(hintText: "Password"),
+              focusNode: _passwordFocus,
+              obscureText: true,
+              onFieldSubmitted: (value) => _submit(),
+              textInputAction: TextInputAction.go,
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter a password';
+                }
+                if (value != "demo") {
+                  return 'Incorrect password';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: Center(
+              child: RaisedButton(
+                onPressed: _submit,
+                child: Text('Sign in', style: TextStyle(color: Colors.black)),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Text(
+                "Sign in with Splits.io is in early access. If you're not sure if you have it, you probably don't."),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/landing_page.dart
+++ lib/widgets/landing_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:splitsio/models/auth.dart';
 
 import 'package:splitsio/models/runner.dart';
+import 'package:splitsio/screens/demo_sign_in.dart';
 import 'package:splitsio/screens/index.dart';
 import 'package:splitsio/widgets/logo.dart';
 
@@ -11,39 +12,49 @@ class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-          children: [
-            Row(
-              children: [
-                Center(
-                  child: Logo(size: 44),
-                )
-              ],
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Center(
+                child: Logo(size: 44),
+              )
+            ],
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+          ),
+          RaisedButton(
+            color: Color(0xFF9146FF),
+            child: const Text(
+              'Sign in with Twitch',
             ),
-          ],
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-        ),
-        floatingActionButton: InkWell(
-            child: FloatingActionButton(
-              child: Icon(Icons.arrow_forward),
-              onPressed: () {
-                Auth.demo = false;
-                Navigator.pushAndRemoveUntil(context,
-                    MaterialPageRoute<void>(builder: (BuildContext context) {
-                  return IndexScreen(runner: Runner.me());
-                }), (route) => false);
-              },
-            ),
-            onLongPress: () {
-              Auth.demo = true;
+            onPressed: () {
+              Auth.demo = false;
               Navigator.pushAndRemoveUntil(context,
                   MaterialPageRoute<void>(builder: (BuildContext context) {
                 return IndexScreen(runner: Runner.me());
               }), (route) => false);
-            }),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat);
+            },
+          ),
+          FlatButton(
+            child: const Text(
+              'Sign in with Splits.io',
+            ),
+            onPressed: () {
+              Auth.demo = false;
+              Navigator.push(context,
+                  MaterialPageRoute<void>(builder: (BuildContext context) {
+                return DemoSignInScreen();
+              }));
+            },
+          )
+        ],
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+      ),
+    );
   }
 }
